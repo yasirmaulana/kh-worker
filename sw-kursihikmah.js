fcf590b77bc2f799e2c9ca22fc6ac721975456a9
Muhammad Yasir Maulana <32666337+yasirmaulana@users.noreply.github.com>
2025-05-02 07:41:24 +0700

    Update sw-kursihikmah.js

--- sw-kursihikmah.js
+++ sw-kursihikmah.js
@@ -1,24 +1,47 @@
-addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
 
-async function handleRequest(request) {
-  return new Response(`
-    self.addEventListener('install', (event) => {
-      console.log('[ServiceWorker] Installed');
-      self.skipWaiting();
-    });
-
-    self.addEventListener('activate', (event) => {
-      console.log('[ServiceWorker] Activated');
-    });
-
-    self.addEventListener('fetch', (event) => {
-      event.respondWith(fetch(event.request));
-    });
-  `, {
-    headers: {
-      'content-type': 'application/javascript'
+    if (url.pathname === "/sw.js") {
+      return new Response(`
+        self.addEventListener('install', (event) => {
+          self.skipWaiting();
+        });
+        self.addEventListener('activate', (event) => {});
+        self.addEventListener('fetch', (event) => {
+          event.respondWith(fetch(event.request));
+        });
+      `, {
+        headers: { 'Content-Type': 'application/javascript' }
+      });
     }
-  });
+
+    if (url.pathname === "/manifest.json") {
+      return new Response(JSON.stringify({
+        name: "Kursi Hikmah",
+        short_name: "Kursi",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0275d8",
+        icons: [
+          {
+            src: "https://yasirmaulana.github.io/my-pwa-icons/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "https://yasirmaulana.github.io/my-pwa-icons/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }), {
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    // Proxy semua permintaan lainnya ke Bubble
+    return fetch("https://kursihikmah.bubbleapps.io" + url.pathname + url.search);
+  }
 }
